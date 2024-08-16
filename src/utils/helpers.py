71ef240c58919523bbbdaefb6ce95568846b2c68
Shrecki <fabrice.guibert@epfl.ch>
2024-08-16 10:55:26 +0200

    State machine improvement to better streamline separation of concerns in code

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,3 +1,4 @@
+from tqdm.notebook import tqdm_notebook
 def print_step(step_name):
     print("Starting {}".format(step_name))
 
@@ -15,24 +16,55 @@ def step_wrapper(f_name, func):
     return wrapped_func
 
 class StateMachine:    
-    def __init__(self, state_seq):
+    def __init__(self, state_seq, state_funs=None):
         assert state_seq is not None
         self.setState(0)
         self._state_seq = state_seq
         self._nstates = len(self._state_seq)
+        self._state_funs = state_funs
         assert isinstance(self._nstates, int)
         assert isinstance(self._state, int)
         assert self._state_seq is not None
+        if state_funs is not None:
+            assert len(state_seq) == len(state_funs)
         
     def getStateName(self):
         return self._state_seq[self._state]
     
     def setState(self,state):
         self._state = state
+        
+    def hasNext(self):
+        return self._state < (self._nstates - 1)
+
     
     def transition(self):
         ret = None
-        if self._state < (self._nstates - 1):
+        if self.hasNext():
             self.setState(self._state + 1)
             ret = self.getStateName()
         return ret
+    
+    def executeStateFun(self,**kwargs):
+        return self._state_funs[self._state](**kwargs)
+    
+    def runSMandFunctions(self, argList, skipSteps):
+        assert len(argList) == self._nstates
+        total_steps = sum(1 for state in self._state_seq if state not in skipSteps)
+        loopStop = False
+        with tqdm_notebook(total=total_steps, desc="Subject Preprocessing", unit="step") as pbar:
+            while(not loopStop):
+                state = self.getStateName()
+            
+                # If the state should be executed, then execute its corresponding function
+                if state not in skipSteps:
+                    print_step(state)
+                    if self._state_funs is not None:
+                        self.executeStateFun(**argList[self._state])
+                    print_success()
+                    pbar.update(1)
+                # Execute state transition regardless  
+                if self.hasNext():
+                    nS = self.transition()
+                else:
+                    loopStop = True
